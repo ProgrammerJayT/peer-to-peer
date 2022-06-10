013729d43aa30bd62f505e6dd1f0ce44a2c680ef
ProgrammerJayT <theanthem8@gmail.com>
2022-06-10 02:18:18 +0200

    10/06

--- private/views/loan/sponsor.view.php
+++ private/views/loan/sponsor.view.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html style="background: url(&quot;assets/img/rm222batch4-mind-18.jpg&quot;);height: 100%;">
+<html style="background: url(&quot;assets/img/rm222batch4-mind-18.jpg&quot;);">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>P2P FinTech (Pty) Ltd</title>
+    <title>Peer-to-peer Admin (copy)</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Advent+Pro">
@@ -33,15 +33,17 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/css/bootstrap-slider.min.css">
+    <link rel="stylesheet" href="assets/css/Multi-step-form.css">
     <link rel="stylesheet" href="assets/css/Navbar---Logo-Middle---Phone-Logo-Middle.css">
     <link rel="stylesheet" href="assets/css/News-Cards.css">
+    <link rel="stylesheet" href="assets/css/Powerful-Calendar.css">
     <link rel="stylesheet" href="assets/css/Slider-Range.css">
     <link rel="stylesheet" href="assets/css/Table-With-Search-1.css">
     <link rel="stylesheet" href="assets/css/Table-With-Search.css">
     <link rel="stylesheet" href="assets/css/Team-with-rotating-cards.css">
 </head>
 
-<body style="background: rgba(255,255,255,0);height: 100%;">
+<body style="background: rgba(255,255,255,0);">
     <section class="d-xl-flex flex-column card-section-imagia" style="background: rgba(255,255,255,0);height: 100%;padding-bottom: 0px;width: 100%;">
         <div class="container" style="height: 100%;width: 100%;">
             <div class="row" style="height: 100%;">
@@ -87,56 +89,9 @@
                                             <form method="post">
                                                 <div class="d-xl-flex justify-content-xl-center mb-3"><input class="form-control" type="number" step="0.1" name="interest" min="0" max="100" placeholder="Interest rate" required="" style="width: 50%;text-align: center;border-radius: 20px;"></div>
                                                 <h2 class="text-center mb-4" style="font-family: Nunito, sans-serif;margin-top: 50px;">Repayment date</h2>
-                                                <div class="row">
-                                                    <div class="col-sm-12 d-xl-flex justify-content-xl-center"><select class="form-select display-inline-block" style="border-bottom-left-radius: 20px;border-top-left-radius: 20px;">
-                                                            <option value="1" selected="">January</option>
-                                                            <option value="2">February</option>
-                                                            <option value="3">March</option>
-                                                            <option value="4">April</option>
-                                                            <option value="5">May</option>
-                                                            <option value="6">June</option>
-                                                            <option value="7">July</option>
-                                                            <option value="8">August</option>
-                                                            <option value="9">September</option>
-                                                            <option value="10">October</option>
-                                                            <option value="11">November</option>
-                                                            <option value="12">December</option>
-                                                        </select><select class="form-select display-inline-block">
-                                                            <option value="1" selected="">1</option>
-                                                            <option value="2">2</option>
-                                                            <option value="3">3</option>
-                                                            <option value="4">4</option>
-                                                            <option value="5">5</option>
-                                                            <option value="6">6</option>
-                                                            <option value="7">7</option>
-                                                            <option value="8">8</option>
-                                                            <option value="9">9</option>
-                                                            <option value="10">10</option>
-                                                            <option value="11">11</option>
-                                                            <option value="12">12</option>
-                                                            <option value="13">13</option>
-                                                            <option value="14">14</option>
-                                                            <option value="15">15</option>
-                                                            <option value="16">16</option>
-                                                            <option value="17">17</option>
-                                                            <option value="18">18</option>
-                                                            <option value="19">19</option>
-                                                            <option value="20">20</option>
-                                                            <option value="21">21</option>
-                                                            <option value="22">22</option>
-                                                            <option value="23">23</option>
-                                                            <option value="24">24</option>
-                                                            <option value="25">25</option>
-                                                            <option value="26">26</option>
-                                                            <option value="27">27</option>
-                                                            <option value="28">28</option>
-                                                            <option value="29">29</option>
-                                                            <option value="30">30</option>
-                                                            <option value="31">31</option>
-                                                        </select><select class="form-select display-inline-block" style="border-top-right-radius: 20px;border-bottom-right-radius: 20px;">
-                                                            <option value="2022" selected="">2022</option>
-                                                            <option value="2023">2023</option>
-                                                        </select></div>
+                                                <div class="d-xl-flex justify-content-xl-center" style="margin-top: 50px;">
+                                                    <div class="d-xl-flex flex-row justify-content-xl-center" style="width: 50%;">
+                                                    <input class="form-control" type="date" name="date" min="<?=date("Y/m/d")?>" style="border-radius: 20px;"></div>
                                                 </div>
                                                 <div class="d-xl-flex justify-content-xl-center" style="margin-top: 50px;">
                                                     <div class="d-xl-flex flex-row align-items-xl-center" style="width: 50%;"><a class="btn d-block w-100" role="button" href="CustomerRequests" style="border-radius: 20px;background: #ff0000;color: rgb(255,255,255);font-weight: bold;margin-right: 20px;margin-left: 20px;">Cancel</a><button class="btn d-block w-100" type="submit" style="border-radius: 20px;background: #004495;color: rgb(255,255,255);font-weight: bold;margin-right: 20px;margin-left: 20px;">Submit</button></div>
@@ -169,6 +124,9 @@
     <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/bootstrap-slider.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+    <script src="assets/js/Multi-step-form.js"></script>
+    <script src="assets/js/Powerful-Calendar.js"></script>
     <script src="assets/js/Range-selector---slider.js"></script>
     <script src="assets/js/Table-With-Search.js"></script>
     <script src="assets/js/theme.js"></script>
